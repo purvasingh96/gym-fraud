de1fca483f657719d1e8a51c5a0a6cd0e7874470
Purva Singh <singhpurva2906@gmail.com>
2019-06-14 01:45:32 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,9 @@
+#!/usr/bin/env python
+from setuptools import setup
 
+setup(name='gym_fraud',
+      version='0.0.1',
+      install_requires=['gym',
+                       'pandas',
+                       'cfg_load']  
+)
