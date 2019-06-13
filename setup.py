0f8143e252ff5e6c79494d8b9af0f6b12e7acd12
purva <singhpurva2906@gmail.com>
2019-06-14 02:19:14 +0530

    master: Registering new gym-fraud environment in openai.

--- setup.py
+++ setup.py
@@ -3,7 +3,5 @@ from setuptools import setup
 
 setup(name='gym_fraud',
       version='0.0.1',
-      install_requires=['gym',
-                       'pandas',
-                       'cfg_load']  
+      install_requires=['gym']
 )
